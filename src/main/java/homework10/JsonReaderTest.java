a62ff0e0d5dadbb7eca0bde60e668cbd733cb9b9
Тимченко Влад <timchenko.vld@gmail.com>
2023-07-15 18:13:45 +0300

    read file and write to json

---
+++ src/main/java/homework10/JsonReaderTest.java
@@ -0,0 +1,11 @@
+package homework10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonReaderTest {
+    public static void main(String[] args) {
+        JsonWriter jsonWriter = new JsonWriter();
+        jsonWriter.readFromFile();
+    }
+}
--- src/main/java/homework10/PhoneNumberValidator.java
+++ src/main/java/homework10/PhoneNumberValidator.java
@@ -5,7 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberValidator {
-    private static final String ABSOLUTE_PATH = "C:\\Users\\Ra1n\\IdeaProjects\\HomeWork\\src\\homework10\\file1.txt";
+    private static final String ABSOLUTE_PATH = "C:\\Users\\Ra1n\\IdeaProjects\\HomeWork\\src\\main\\" +
+            "java\\homework10\\file1.txt";
 
     public void validateNumbersFromFile() {
         File file = new File(ABSOLUTE_PATH);
