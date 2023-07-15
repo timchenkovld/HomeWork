5dd511eb2bcb7bcc248dbfe92b7be0ddcb479df0
Тимченко Влад <timchenko.vld@gmail.com>
2023-07-15 18:16:46 +0300

    read file and write to json

--- src/main/java/homework10/JsonWriterTest.java
+++ src/main/java/homework10/JsonWriterTest.java
@@ -1,9 +1,6 @@
 package homework10;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class JsonReaderTest {
+public class JsonWriterTest {
     public static void main(String[] args) {
         JsonWriter jsonWriter = new JsonWriter();
         jsonWriter.readFromFile();
